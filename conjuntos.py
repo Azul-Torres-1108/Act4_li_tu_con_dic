672775f77b22c63b4bfa29670d08d9714a1df9e7
Azul-Torres-1108 <167554917+Azul-Torres-1108@users.noreply.github.com>
2024-09-06 12:10:03 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,8 @@
+Misamigas = {"Diana", "Andy", "Leysi"}
+print(Misamigas)
+
+print("Uso de for en conjuntos")
+Misamigos = {"Najera", "Rodo", "Ivan"}
+
+for x in Misamigos:
+    print(x)
---
+++ diccionarios.py
@@ -0,0 +1,8 @@
+## Ejemplo de diccionario
+Micarro = {
+  "Marca": "Ford",
+  "Modelo": "Mustang",
+  "Año": 1999
+}
+for x in Micarro:
+    print(x)
